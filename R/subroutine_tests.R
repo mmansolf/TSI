3920874ce2479cd768ea83d8f9e9adc466e1c97a
Maxwell Mansolf <maxwell.mansolf>
2022-03-20 16:25:15 -0700

    hihi

--- R/subroutine_tests.R
+++ R/subroutine_tests.R
@@ -1,1 +1,17 @@
-stop('Hammer time')
+library(testthat)
+
+###################
+# ALL SUBROUTINES #
+test_that("All the subroutine functions exist", {
+  expect_true(exists('PrepareMemParam'))
+  expect_true(exists('generateRandomMultivarRegreParam'))
+  expect_true(exists('createMatrixonW'))
+  expect_true(exists('completeGmatrix'))
+  expect_true(exists('isSymmetric'))
+  expect_true(exists('sweep'))
+  expect_true(exists('generateMissingvalue'))
+  expect_true(exists('MIEC'))
+})
+
+###################
+# PrepareMemParam #
