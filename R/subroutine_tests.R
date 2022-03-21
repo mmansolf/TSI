12194d2d9b824bba2b1f8949f265af9d23afe95d
Maxwell Mansolf <maxwell.mansolf>
2022-03-20 17:04:52 -0700

    Tests work!

--- R/subroutine_tests.R
+++ R/subroutine_tests.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env C:/Program Files/R/R-4.0.2/bin/Rscript.exe
 
-library(testthat,
-        lib.loc=c("C:/Users/Max/Documents/R/win-library/4.0",
-                  "C:/Program Files/R/R-4.0.2/library"))
+.libPaths(c("C:/Users/Max/Documents/R/win-library/4.0",
+            "C:/Program Files/R/R-4.0.2/library"))
+library(testthat)
 source('R/subroutines.R')
 
 ###################
@@ -14,7 +14,6 @@ test_that("All the subroutine functions exist", {
   expect_true(exists('completeGmatrix'))
   expect_true(exists('isSymmetric'))
   expect_true(exists('sweep'))
-  expect_true(exists('SomethingElse'))
   expect_true(exists('generateMissingvalue'))
   expect_true(exists('MIEC'))
 })
